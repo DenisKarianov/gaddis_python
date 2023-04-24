db7effc0c1ba42c7d3c7eeb595dde1f2b3771021
karianov.d.n <karianov.d.n@gmail.com>
2023-04-24 18:06:36 +0200

    ch.14 SQLite3 exercises

--- 14.3.students_db.py
+++ 14.3.students_db.py
@@ -1,27 +1,41 @@
-# program to work with phonebook database
+# program to work with students' database
 import sqlite3
 
 # constants
 MIN_CHOICE = 1
-MAX_CHOICE = 5
+MAX_CHOICE = 6
 ADD = 1
 FIND = 2
 CHANGE = 3
 DELETE = 4
-EXIT = 5
-DB = 'phonebook.db'
+SHOW_ALL = 5
+EXIT = 6
+
+MAIN_MIN_CHOICE = 1
+MAIN_MAX_CHOICE = 4
+MAIN_MAJORS = 1
+MAIN_DEPS = 2
+MAIN_STUDENTS = 3
+MAIN_EXIT = 4
+DB = 'student_info.db'
 
 
 def main():
     create_db()
     choice = 0
-    while choice != EXIT:
-        display_menu()
-        choice = get_menu_choice()
-        if choice == ADD:
-            add()
-        elif choice == FIND:
-            find()
+    while choice != MAIN_EXIT:
+        display_main_menu()
+        choice = get_menu_choice(MAIN_MIN_CHOICE, MAIN_MAX_CHOICE)
+        if choice == MAIN_MAJORS:
+            choice = 0
+            display_menu("Majors")
+            choice = get_menu_choice(MIN_CHOICE, MAX_CHOICE)
+            while choice != EXIT:
+                if choice == ADD:
+                    add(0)
+                elif choice == FIND:
+                    find()
+
         elif choice == CHANGE:
             change()
         elif choice == DELETE:
@@ -33,7 +47,11 @@ def create_db():
     try:
         conn = sqlite3.Connection(DB)
         cur = conn.cursor()
-        cur.execute('''CREATE TABLE IF NOT EXISTS Entries (Name TEXT, Phone TEXT)''')
+        cur.execute('''CREATE TABLE IF NOT EXISTS Majors (MajorID INTEGER PRIMARY KEY NOT NULL, Name TEXT)''')
+        cur.execute('''CREATE TABLE IF NOT EXISTS Departments (DeptID INTEGER PRIMARY KEY NOT NULL, Name TEXT)''')
+        cur.execute('''CREATE TABLE IF NOT EXISTS Students (StudentID INTEGER PRIMARY KEY NOT NULL, Name TEXT,
+                    MajorID INTEGER, DeptID INTEGER, FOREIGN KEY (MajorID) REFERENCES Majors(MajorID),
+                    FOREIGN KEY (DeptID) REFERENCES Departments(DeptID))''')
     except sqlite3.Error as err:
         print(err)
     finally:
@@ -41,47 +59,76 @@ def create_db():
             conn.close()
 
 
-def display_menu():
-    print("\n--- Phonebook ---")
+def display_main_menu():
+    print("\n--- Database management ---")
+    print("1. Majors.")
+    print("2. Departments.")
+    print("3. Students.")
+    print("4. Quit program.")
+
+
+def display_menu(db_name):
+    print(f"\n--- {db_name} ---")
     print("1. Add entry.")
     print("2. Find entry.")
     print("3. Change entry.")
     print("4. Delete entry.")
-    print("5. Quit program.")
+    print("5. Show all entries.")
+    print("6. Exit to previous menu.")
 
 
-def get_menu_choice():
+def get_menu_choice(min_choice, max_choice):
     loop_flag = 0
     while loop_flag < 2:
         choice = input("Enter your menu choice: ")
         if choice.isdigit():
             choice = int(choice)
             loop_flag += 1
-            if MIN_CHOICE <= choice <= MAX_CHOICE:
+            if min_choice <= choice <= max_choice:
                 loop_flag += 1
             else:
-                print(f"Your choice can be integer from {MIN_CHOICE} to {MAX_CHOICE} only.")
+                print(f"Your choice can be integer from {min_choice} to {max_choice} only.")
         else:
             print("Your choice can be a integer only.")
     return choice
 
 
-def add():
+# mode: 0 Majors, 1 Departments, 2 Students
+def add(mode):
     conn = None
     try:
         conn = sqlite3.connect(DB)
         cur = conn.cursor()
-        loop_flag = True
-        while loop_flag:
-            name = input("Enter a name: ")
-            phone = input("Enter a phone number: ")
-            cur.execute('''INSERT INTO Entries (Name, Phone)
-                                VALUES (?, ?)''', (name, phone))
+        cur.execute('PRAGMA foreign_keys=ON')  # turn on foreign keys support
+        if mode == 0:
+            name = input("Enter major's name: ")
+            cur.execute('''INSERT INTO Majors (Name) VALUES (?)''', (name,))
             conn.commit()
-            print(f"Entry with name {name} and phone number {phone} was added.")
-            again = input("Add one more entry (y/n)?")
-            if again.lower() != 'y':
-                loop_flag = False
+            print(f"Entry with name {name} was added.")
+        elif mode == 1:
+            name = input("Enter department's name: ")
+            cur.execute('''INSERT INTO Departments (Name) VALUES (?)''', (name,))
+            conn.commit()
+            print(f"Entry with name {name} was added.")
+        elif mode == 2:
+            name = input("Enter student's name: ")
+            # validation loop for integer MajorID
+            loop_flag = True
+            while loop_flag:
+                majorid = input("Enter student's MajorID: ")
+                if majorid.isdigit():
+                    majorid = int(majorid)
+                    loop_flag = False
+            # validation loop for integer DeptID
+            loop_flag = True
+            while loop_flag:
+                deptid = input("Enter student's DeptID: ")
+                if deptid.isdigit():
+                    deptid = int(deptid)
+                    loop_flag = False
+            cur.execute('''INSERT INTO Students (Name, MajorID, DeptID) VALUES (?, ?, ?)''', (name, majorid, deptid))
+            conn.commit()
+            print(f"Student with name {name} was added.")
     except sqlite3.Error as err:
         print(err)
     finally:
@@ -94,6 +141,7 @@ def find():
     try:
         conn = sqlite3.connect(DB)
         cur = conn.cursor()
+        # check quantity of columns in table to
         search = input("Enter a name or phone number to find: ")
         search = '%' + search.lower() + '%'
         cur.execute('''SELECT Name, Phone FROM Entries WHERE lower(Name) LIKE ? OR
